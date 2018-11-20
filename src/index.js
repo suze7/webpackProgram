4455bf4043afb66f49d6709b89dac7914daea1d8
suzexiong <734531289@qq.com>
2018-11-20 17:15:48 +0800

    开发

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo.png';
-import Data from './data.xml';
+// import './style.css';
+// import Icon from './logo.png';
+// import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+  // element.classList.add('hello');
+  // var myIcon = new Image();
+  // myIcon.src = Icon;
+  // element.appendChild(myIcon);
+  // console.log(Data);
+  var btn = document.createElement('button');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,41 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      }
-    ]
-  }
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ]
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: ['style-loader', 'css-loader']
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/,
+  //       use: ['file-loader']
+  //     },
+  //     {
+  //       test: /\.xml$/,
+  //       use: ['xml-loader']
+  //     }
+  //   ]
+  // }
 };
